3ba7a95ad82a21ee99a94022d5c73627ce8d5962
Brainer21 <jarisl.ultio.21@gmail.com>
2025-04-21 11:17:52 +0100

    added missing migrations & seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,11 +12,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $this->call([
+            LanguageSeeder::class,
+            RoleSeeder::class,
+            FeatureSeeder::class,
+            AdminSeeder::class,
+            UserSeeder::class,
+            MethodSeeder::class,
+        ]);
     }
 }
